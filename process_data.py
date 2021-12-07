8364f30f5c888068c7bd6f98f8bd455a2fefba10
Ryan Goggins <ryanngoggins@gmail.com>
2021-12-06 20:57:29 -0500

    More data processing work

--- process_data.py
+++ process_data.py
@@ -1,1 +1,35 @@
-usa_url = 'https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-Jurisdi/unsk-b7fc'
+# import json
+# fp = "./data/unsk-b7fc.json.1"
+# data = None
+# with open(fp, "r") as read_file:
+#     data = json.load(read_file)
+#
+# if (not data):
+#     print("Data could not be loaded")
+#     exit(0)
+# else:
+#     print("Data loaded into dict")
+#
+# print("Type of external structure:",type(data))
+# print("Length of data: " + str(len(data)))
+#
+#
+# print("Type of first internal structure",type(data[0]))
+# print("Keys of first internal structure",data[0].keys())
+import pandas as pd
+
+df = pd.read_csv('./data/COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv')
+
+# if ( df):
+#     print("Unable to read corrupted csv")
+# else:
+print("Read csv which has " + str(df.shape) + " dims.")
+
+print("CSV Data:")
+print(df.head())
+
+
+# Load data into df and perform inference
+
+
+# Create new dictionary with additional values
